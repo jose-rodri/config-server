f7adedd5dbdab7aff87a053cb77dd648f9e45f11
JoseR <rodri@gmail.com>
2019-09-24 20:18:52 -0500

    Config-server completado

---
+++ spring-boot-proyecto-everis-config-server/src/main/java/jose/rodriguez/everis/peru/app/SpringBootProyectoEverisConfigServerApplication.java
@@ -0,0 +1,15 @@
+package jose.rodriguez.everis.peru.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringBootProyectoEverisConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProyectoEverisConfigServerApplication.class, args);
+	}
+
+}
